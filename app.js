3bea96bd33c4194ad09e1264c902cab7a13d18fc
labeeblabu <labeelabu7012488568@gmail.com>
2022-01-30 14:57:30 +0530

    added e1

--- app.js
+++ app.js
@@ -4,12 +4,16 @@
 // server.listen(3000)
 //______________________________________________________________
 
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
-
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
+const app=express();
+app.use((req,res,next) => {
+    console.log(`in the middleware`);
+    // res.send(`<h1> hello world!</h1>`);
+    next();
+})
+app.use((req,res,next) => {
+    console.log(`in another middleware`);
+    res.send(`<h1> hello world2!</h1>`);
+})
+app.listen(3000)
